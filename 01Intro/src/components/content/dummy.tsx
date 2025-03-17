51877718421d684dfacebdd3ca761bf1c338c23b
Amol Sargar <amol1501966@mastek.com>
2025-03-18 00:24:56 +0530

    Ojet learning

---
+++ 01Intro/src/components/content/dummy.tsx
@@ -0,0 +1,11 @@
+
+export function Dummy() {
+    console.log("inside dummy")
+    return (
+      <div>
+        <p>
+          hii from dummy
+        </p>
+      </div>
+    );
+  };
---
+++ 01Intro/src/components/dashboard/index.tsx
@@ -0,0 +1,80 @@
+import { h } from "preact";
+import ArrayDataProvider = require('ojs/ojarraydataprovider');
+import { ojSelectSingle } from 'ojs/ojselectsingle';
+import 'ojs/ojselectsingle';
+import 'ojs/ojchart'
+import { useMemo, useState } from "preact/hooks";
+import * as data from 'text!./data.json';
+
+
+
+
+//data
+const moviesDetails = JSON.parse(data);
+const movies = [
+    { value: 'Avengers: Endgame', label: 'Avengers: Endgame' },
+    { value: 'Titanic', label: 'Titanic' },
+    { value: 'Avatar', label: 'Avatar' },
+    { value: 'Joker', label: 'Joker' },
+    { value: 'The Lion King', label: 'The Lion King' },
+    { value: 'The Dark Knight', label: 'The Dark Knight' },
+    { value: 'It', label: 'It' },
+    { value: 'Inception', label: 'Inception' },
+    { value: 'Black Panthe', label: 'Black Panthe' },
+    { value: 'Frozen II', label: 'Frozen II' }
+];
+
+
+
+
+export function Dashboard() {
+    const [selectedMovie, setSelectedMovie] = useState<string>('Avengers: Endgame');
+    // const [selectePrevMovie, setSelectedPrevMovie] = useState<string>(null);
+
+    const moviesCollectionDP = useMemo(() => {
+
+        //moviesDetails.movies[selectedMovie]
+        return new ArrayDataProvider(moviesDetails[selectedMovie], {
+            keyAttributes: 'id'
+        });
+
+    }, [selectedMovie, setSelectedMovie]);
+    const activitiesDP = new ArrayDataProvider(movies, {
+        keyAttributes: 'value'
+    });
+
+    const valueChanged = (event: ojSelectSingle.valueChanged<string, string>) => {
+        //setSelectedMovie(event.detail);
+        // setSelectedPrevMovie(event.detail.previousValue);
+        console.log(event.detail.value);
+        console.log(event.detail);
+        
+    }
+    
+return (
+    <>
+    <oj-select-single
+        id="select1"
+        labelHint="Select your favorite movie"
+        labelEdge="inside"
+        class="oj-form-control-max-width-md"
+        data={activitiesDP}
+        itemText={"label"}
+        onvalueChanged={valueChanged}
+    ></oj-select-single>
+    <h3>{selectedMovie}</h3>
+
+    <div>
+        <oj-chart
+            id="pieChart"
+            type="pie"
+            data={moviesCollectionDP}
+            animation-on-display="auto"
+            animation-on-data-change="auto"
+        >
+
+        </oj-chart>
+    </div>
+    </>
+);
+}
